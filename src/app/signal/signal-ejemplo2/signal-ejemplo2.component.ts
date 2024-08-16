d4aa80e5ffbaff85ef223db0c360afad7e7d2c22
KrankVegan <martinezcawork@gmail.com>
2024-08-16 15:18:45 -0500

    fixed ejemplo 2, plus added ejemplo 6 and 7 where a simple shopping cart and writable signals are being displayed

--- src/app/signal/signal-ejemplo2/signal-ejemplo2.component.ts
+++ src/app/signal/signal-ejemplo2/signal-ejemplo2.component.ts
@@ -1,19 +1,38 @@
+import { Component, signal, computed } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-signal-ejemplo2',
   standalone: true,
-  imports: [CommonModule, ],
+  imports: [CommonModule],
   templateUrl: './signal-ejemplo2.component.html',
-  styleUrl: './signal-ejemplo2.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  styleUrls: ['./signal-ejemplo2.component.scss']
 })
 export default class SignalEjemplo2Component {
-  contador = signal(0)
-  /*
-  constructor() {
-    setInterval( () => this.contador.set(this.contador() + 1 ), 1000); 
+  count = signal(0);
+  isRunning = signal(false);
+
+  incrementedCount = computed(() => this.count() + 1);
+
+  startAutoIncrement() {
+    if (!this.isRunning()) {
+      this.isRunning.set(true);
+      this.incrementLoop();
+    }
   }
-    */
+
+  stopAutoIncrement() {
+    this.isRunning.set(false);
+  }
+
+  resetCount() {
+    this.count.set(0);
+  }
+
+  private incrementLoop() {
+    if (this.isRunning()) {
+      this.count.update(val => val + 1);
+      setTimeout(() => this.incrementLoop(), 1000);
+    }
+  }
 }
--- src/app/signal/signal-ejemplo6/signal-ejemplo6.component.ts
+++ src/app/signal/signal-ejemplo6/signal-ejemplo6.component.ts
@@ -11,12 +11,10 @@ import {toSignal} from '@angular/core/rxjs-interop'
   changeDetection: ChangeDetectionStrategy.OnPush 
 })
 export default class SignalEjemplo6Component {
- /* counter$ = interval(1000);        
-  counter = toSignal(this.counter$);
-
-  counter2$ = interval(1000)
+  counter$ = interval(1000).pipe(take(10)); // Limitar a 10 emisiones
+  counter = toSignal(this.counter$)
+  counter2$ = interval(1000).pipe(take(10)); // Limitar a 10 emisiones;
   counterConValorInicial = toSignal(this.counter2$, {initialValue: 0})
-  */
-  counter3$ = interval(1000).pipe(take(6));
-  counterUpTo5 = toSignal(this.counter3$);
+  counter3$ = interval(1000).pipe(take(10)); // Limitar a 10 emisiones
+  counterUpTo5 = toSignal(this.counter3$)
 }
